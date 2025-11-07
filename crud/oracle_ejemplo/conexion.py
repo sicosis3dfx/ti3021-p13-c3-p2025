0683d1bd63be7ea00ce933e920d6813d6b84aece
sicosis3dfx <sicosis3dfx@outlook.com>
2025-11-07 11:03:04 -0300

    Update conexion.py

--- crud/oracle_ejemplo/conexion.py
+++ crud/oracle_ejemplo/conexion.py
@@ -2,11 +2,18 @@ import oracledb
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
-with oracledb.connect(user=username, password=password, dsn=dsn) as connection:
+
+with oracledb.connect(
+     user=username, 
+     password=password, 
+     dsn=dsn
+     ) as connection:
     with connection.cursor() as cursor:
         sql = "select sysdate from dual"
-        for r in cursor.execute(sql):
-                print(r)
+        for row in cursor.execute(sql):
+            for column in row:
+                print(column)
