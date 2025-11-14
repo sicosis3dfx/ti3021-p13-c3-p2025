7bc8761f86618bd8deab8b5abc47ee452a23625c
sicosis3dfx <sicosis3dfx@outlook.com>
2025-11-14 10:29:41 -0300

    MODELO SQL
    DATOS

---
+++ crud/oracle_ejemplo/MODELO_BDD.sql
@@ -0,0 +1,40 @@
+
+            CREATE TABLE Evento (
+                idevento INTEGER PRIMARY KEY,
+                nombre VARCHAR(50) NOT NULL,
+                fecha DATE,
+                lugar VARCHAR(50)
+            )
+
+            CREATE TABLE Participante (
+                idparticipante INTEGER PRIMARY KEY,
+                nombre VARCHAR(50) NOT NULL,
+                rut VARCHAR(10) UNIQUE,
+                edad INTEGER,
+                numeroinscripcion INTEGER
+            )
+
+            CREATE TABLE Atleta (
+                idparticipante INTEGER PRIMARY KEY,
+                disciplina TEXT,
+                marca NUMERIC,
+                FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)
+            )
+
+            CREATE TABLE Entrenador (
+                identrenador INTEGER PRIMARY KEY,
+                equipo VARCHAR(50)
+            )
+
+            CREATE TABLE Juez (
+                idjuez INTEGER PRIMARY KEY,
+                especialidad (50)
+            )
+
+            CREATE TABLE Inscripcion (
+                idevento INTEGER,
+                idparticipante INTEGER,
+                PRIMARY KEY (idevento, idparticipante),
+                FOREIGN KEY (idevento) REFERENCES Evento(idevento),
+                FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)
+            )
--- crud/oracle_ejemplo/ejemplo_crud.py
+++ crud/oracle_ejemplo/ejemplo_crud.py
@@ -10,71 +10,64 @@ password = os.getenv("ORACLE_PASSWORD")
 def get_connection():
     return oracledb.connect(user=username, password= password, dsn= dsn)
 
+def create_schema (query):
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                print("Tabla 'personas' creada.")
+    except oracledb.DatabaseError as error:
+                print(f"No se pudo crear la tabla: {error}")
 
 def create_table_personas():
-    query = (
-        "CREATE TABLE personas ("
-        "rut VARCHAR2(50) PRIMARY KEY,"
-        "nombres VARCHAR2(200),"
-        "apellidos VARCHAR2(200),"
-        "fecha_nacimiento DATE,"
-        "cod_area VARCHAR2(20),"
-        "numero_telefono VARCHAR2(50)"
-        ")"
-    )
+    tables = [
+            (
+                "CREATE TABLE Evento ("
+                "idevento INTEGER PRIMARY KEY,"
+                "nombre VARCHAR(50) NOT NULL,"
+                "fecha DATE,"
+                "lugar VARCHAR(50)"
+            ),
+            (
+                "CREATE TABLE Participante ("
+                "idparticipante INTEGER PRIMARY KEY,"
+                "nombre VARCHAR(50) NOT NULL,"
+                "rut VARCHAR(10) UNIQUE,"
+                "edad INTEGER,"
+                "numeroinscripcion INTEGER"
+            ),
+            (
+                "CREATE TABLE Atleta ("
+                "idparticipante INTEGER PRIMARY KEY,"
+                "disciplina TEXT,"
+                "marca NUMERIC,"
+                "FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)"
+            ),
+            (
+                "CREATE TABLE Entrenador ("
+                "identrenador INTEGER PRIMARY KEY,"
+                "equipo VARCHAR(50)"
+            ),
+            (
+                "CREATE TABLE Juez ("
+                "idjuez INTEGER PRIMARY KEY,"
+                "especialidad (50)"
+            ),
+            (
+                "CREATE TABLE Inscripcion ("
+                "idevento INTEGER,"
+                "idparticipante INTEGER,"
+                "PRIMARY KEY (idevento, idparticipante),"
+                "FOREIGN KEY (idevento) REFERENCES Evento(idevento),"
+                "FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)"
+            ),
+        ]
 
-    try:
-        with get_connection() as conn:
-            with conn.cursor() as cur:
-             cur.execute(query)
-            print("Tabla 'personas' creada.")
-    except oracledb.DatabaseError as error:
-        print(f"No se pudo crear la tabla: {error}")
-
-        [
-            -- 1. Tabla Evento
-            CREATE TABLE Evento (
-                idevento INTEGER PRIMARY KEY,
-                nombre VARCHAR(50) NOT NULL,
-                fecha DATE,
-                lugar TEXT
-            );
-
-            -- 2. Tabla Participante
-            CREATE TABLE Participante (
-                idparticipante INTEGER PRIMARY KEY,
-                nombre VARCHAR(50) NOT NULL,
-                rut VARCHAR(10) UNIQUE,
-                edad INTEGER,
-                numeroinscripcion INTEGER
-            );
-
-            -- 3. Tabla Atleta (Herencia de Participante)
-            CREATE TABLE Atleta (
-                idparticipante INTEGER PRIMARY KEY,
-                disciplina TEXT,
-                marca NUMERIC,
-                FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)
-            );
-
-            -- 4. Tabla Entrenador
-            CREATE TABLE Entrenador (
-                identrenador INTEGER PRIMARY KEY,
-                equipo TEXT
-            );
-
-            -- 5. Tabla Juez
-            CREATE TABLE Juez (
-                idjuez INTEGER PRIMARY KEY,
-                especialidad TEXT
-            );
-
-            -- Tabla de Relación (Inscripción/Participación)
-            CREATE TABLE Inscripcion (
-                idevento INTEGER,
-                idparticipante INTEGER,
-                PRIMARY KEY (idevento, idparticipante),
-                FOREIGN KEY (idevento) REFERENCES Evento(idevento),
-                FOREIGN KEY (idparticipante) REFERENCES Participante(idparticipante)
-            );
-            ]
+        for query in tables:
+            try:
+                with get_connection() as conn:
+                    with conn.cursor() as cursor:
+                cursor.execute(query)
+                print("Tabla 'personas' creada.")
+            except oracledb.DatabaseError as error:
+                print(f"No se pudo crear la tabla: {error}")
