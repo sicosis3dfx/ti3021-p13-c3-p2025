144b44584ae2c18ba7ef7f42ae5dfacf93bb708c
sicosis3dfx <sicosis3dfx@outlook.com>
2025-12-12 13:58:56 -0300

    Update main.py

--- crud/oracle_ejemplo/autentificacion/main.py
+++ crud/oracle_ejemplo/autentificacion/main.py
@@ -1,63 +1,65 @@
 # Conectarnos a la base de datos
 import oracledb
-#Rescatar variables de entorno
+# Rescatar variables de entorno
 import os
 from dotenv import load_dotenv
-# Implementar hasheo de contraseñas
+# Implementar hasheo de contraeñas
 import bcrypt
-# Importar el tipo de dato opcional 
+# Importar el tipo de dato Opcional
 from typing import Optional
-# Peticiones HTTP
-import datetime
+# Implementar peticiones HTTP
 import requests
+# Importar liberia de fechas
+import datetime
 # Cargar las variables desde el archivo .env
 load_dotenv()
-# Rescatar las credenciales de conexión de Oracledb
+# Rescatar las credenciales de conexion con Oracle
 username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
 class Database:
     def __init__(self, username, password, dsn):
-            self.username = username, 
-            self.password = password, 
-            self.dsn = dsn
+        self.username = username
+        self.password = password
+        self.dsn = dsn
     def get_connection(self):
         return oracledb.connect(user=self.username, password=self.password, dsn=self.dsn)
     def create_all_tables(self):
         pass
-    def query(self, sentence: str, parameters: Optional [dict] = None):
+    def query(self, sentence: str, parameters: Optional[dict] = None):
         print(f"Ejecutando query:\n{sentence}\nParametros:\n{parameters}")
         try:
             with self.get_connection() as connection:
                 with connection.cursor() as cursor:
                     resultado = cursor.execute(sentence, parameters)
                     return resultado
-                for fila in resultado:
-                    print(fila)
-            connection.commit()
+                connection.commit()
         except oracledb.DatabaseError as error:
-            print(f"Hubo un error con la base de datos: {error}")
-# Genera ña autentificación
+            print(f"Hubo un error con al base de datos:\n{error}")
+
+
+# Generar autenticacion
 class Auth:
     @staticmethod
     def register(db: Database, username: str, password: str):
         salt = bcrypt.gensalt(12)
-        hashed_password = bcrypt.hashpw(password.salt)
+        hashed_password = bcrypt.hashpw(password,salt)
         usuario = {
+            "id": 1,
             "username": username,
             "password": hashed_password
         }
-        db.query(   
-            "INSERT INTO USERS(id, usernamne, password) VALUES (: id, :username, :password)",
+
+        db.query(
+            "INSERT INTO USERS(id,username,password) VALUES (:id,:username:password)",
             usuario
         )
-    
     @staticmethod
-    def login(db: Database, username: str, password: str):
-        usuario = db:query(
+    def login(db: Database, username: str, password: str) -> bool:
+        resultado = db.query(
             "SELECT * FROM USERS WHERE username = :username",
-            {"username": username}
+            {"username" : username}
         )
 
         for usuario in resultado:
@@ -65,35 +67,31 @@ class Auth:
             return bcrypt.checkpw(password, password_user)
 
 class Finance:
-    def __init__(self, base_url: str):
+    def __init__(self, base_url: str = "https://mindicador.cl/api"):
         self.base_url = base_url
-    def get_uf(self, fecha: str = None):
+    def get_indicator(self, indicator: str = None, fecha:str=None):
+        if not indicator:
+            return print("Indicador faltante")
         if not fecha:
             year = datetime.datetime.now().year
             month = datetime.datetime.now().month
             day = datetime.datetime.now().day
-            fecha = f"{year}-{month}-{day}"
-        url = f"{self.base_url}/uf/{fecha}"
+            fecha = f"{day}-{month}-{year}"
+        url = f"{self.base_url}/{indicator}/{fecha}"
         data = requests.get(url=url).json()
-        pass
-    def get_uf():
-        pass
-    @staticmethod
-    def get_utm():
-        pass
-    @staticmethod
-    def get_dollar():
-        pass
-    @staticmethod
-    def get_euro():
-        pass
-    @staticmethod
-    def get_ipc():
-        pass
-    @staticmethod
-    def get_ivp():
-        pass
+        print(data['serie'][0]['valor']) 
+    def get_uf(self, fecha: str = None):
+        self.get_indicator("uf", fecha)
+    def get_ivp(self, fecha: str = None):
+        self.get_indicator("ivp", fecha)
+    def get_ipc(self, fecha: str = None):
+        self.get_indicator("ipc", fecha)
+    def get_utm(self, fecha: str = None):
+        self.get_indicator("utm", fecha)
+    def get_usd(self, fecha: str = None):
+        self.get_indicator("dolar", fecha)
+    def get_eur(self, fecha: str = None):
+        self.get_indicator("euro", fecha)
 
 if __name__ == "__main__":
-    db = Database(username=username, password=password, dsn=dsn)
-    db.query("")
+    indicadores = Finance()
